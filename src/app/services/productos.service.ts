050c18302b3bc36b7bbea209b722832e947797f7
Rafa <rk.arillo@mnemo.com>
2019-05-29 15:43:02 +0200

    Fin de la sección 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,6 +9,7 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor( private http: HttpClient ) {
 
@@ -18,13 +19,61 @@ export class ProductosService {
 
   private cargarProductos(){
 
-    this.http.get('https://angular-html-7fa90.firebaseio.com/productos_idx.json')
-      .subscribe( (resp: Producto[]) => {
+    return new Promise( ( resolve, reject ) => {
+      
+      this.http.get('https://angular-html-7fa90.firebaseio.com/productos_idx.json')
+          .subscribe( (resp: Producto[]) => {
+            this.productos = resp;     
+            this.cargando = false; 
+            resolve();
+          });
 
-        console.log(resp);
-        this.productos = resp;     
-        this.cargando = false; 
+    });
 
-      });
+
+    
   }
+
+  getProducto ( id: string ) {
+
+    return this.http.get(`https://angular-html-7fa90.firebaseio.com/productos/${ id }.json`)
+
+  }
+
+  buscarProducto( termino: string) {
+
+    if ( this.productos.length === 0 ) {
+      // Cargar productos
+      this.cargarProductos().then( () => {
+        // Ejecutar después de tener los productos
+        // Aplicar filtro
+        this.filtarProductos( termino );
+      });
+
+    } else {
+      // Aplicar el filtro
+      this.filtarProductos( termino );
+    }
+
+  }
+
+  private filtarProductos( termino: string ) {
+
+    //console.log(this.productos);
+    this.productosFiltrado = [];
+
+    termino = termino.toLowerCase();
+
+    this.productos.forEach( prod => {
+
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+
+      if( prod.categoria.indexOf( termino )  >= 0 || tituloLower.indexOf( termino ) >= 0 ) {
+        this.productosFiltrado.push( prod );
+      }
+
+    });
+  
+  }
+
 }
